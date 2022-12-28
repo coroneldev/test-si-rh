d42f62e027154db7164eff208a5011ca00ed6582
coroneldev <rcoroneldeveloper@gmail.com>
2022-12-28 11:55:08 -0400

    Creacion de datos laborales

---
+++ database/migrations/2022_12_28_154946_create_rh_cl_organismos_financiadores_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rh_cl_organismos_financiadores', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rh_cl_organismos_financiadores');
+    }
+};
